d9ec4ebe80e909a87b77ae0629d7e43d6a60ea03
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-12-19 10:19:55 +0200

    Finish faq

--- src/pages/CruisePage/sections/faq/faq.tsx
+++ src/pages/CruisePage/sections/faq/faq.tsx
@@ -1,13 +1,39 @@
-import { Container, FaqComponent } from '../../../../components'
+import { useState } from 'react'
+
+import { Accordion, Container, Heading } from '../../../../components'
+import { DataFaq } from '../../../../fakedata'
 
 import styles from './faq.module.scss'
 
 export const FAQ = () => {
-	
+	const [activeId, setActive] = useState<number | null>(null)
+
+	const handleToggle = (idx: number) => {
+		if (activeId === idx) {
+			setActive(null)
+		} else {
+			setActive(idx)
+		}
+	}
+
 	return (
 		<Container width="full" className={styles.component}>
 			<div className={styles.container}>
-				<FaqComponent />
+				<Heading as="h4" className={styles.title}>
+					ЧАСТО ЗАДАВЕМЫЕ ВОПРОСЫ
+				</Heading>
+				{DataFaq.map((faq, idx) =>
+					<Accordion
+						key={idx}
+						componentClass={styles.accordionItem}
+						titleClass={styles.accordionTitle}
+						arrowClass={styles.accordionArrow}
+						textClass={styles.accordionSubtitle}
+						activeId={activeId}
+						handleToggle={handleToggle}
+						faq={faq}
+					/>
+				)}
 			</div>
 		</Container>
 	)
