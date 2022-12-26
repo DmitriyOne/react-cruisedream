8fcf1037fd15d74608393e4189072c47c487dabe
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-12-26 12:24:31 +0200

    Finish yourCruise

--- src/components/Cruise/CruiseDates/CruiseDates.tsx
+++ src/components/Cruise/CruiseDates/CruiseDates.tsx
@@ -11,13 +11,17 @@ import styles from './cruise-dates.module.scss'
 import classNames from 'classnames'
 
 interface IProps {
+	classComponent?: string
 	classNameText?: string
 	classNameArrow?: string
+	isOnlyStartDate?: boolean
 }
 
 export const CruiseDates: FC<IProps> = ({
+	classComponent,
 	classNameText,
-	classNameArrow
+	classNameArrow,
+	isOnlyStartDate = false
 }) => {
 	const { isMobile } = useWindowSize()
 	const [idTooltip, setIdTooltip] = useState('')
@@ -26,27 +30,49 @@ export const CruiseDates: FC<IProps> = ({
 		setIdTooltip('my-dates-tooltip-' + 1)
 	}, [])
 
+	const classNameComponent = classNames(classComponent, styles.component)
+
 	return (
-		<div id={idTooltip} className={styles.component}>
-			{isMobile &&
-				<span className={styles.icon}>
-					<img
-						src={calendarIcon}
-						alt="Calendar Icon"
-					/>
-				</span>
+		<>
+			{isOnlyStartDate
+				?
+				<div className={classNameComponent}>
+					{isMobile &&
+						<span className={styles.icon}>
+							<img
+								src={calendarIcon}
+								alt="Calendar Icon"
+							/>
+						</span>
+					}
+					<span className={classNames(classNameText, styles.text)}>
+						01.03.23 - 08.03.23
+					</span>
+				</div>
+				:
+				<div id={idTooltip} className={classNameComponent}>
+					{isMobile &&
+						<span className={styles.icon}>
+							<img
+								src={calendarIcon}
+								alt="Calendar Icon"
+							/>
+						</span>
+					}
+					<MyTooltip isDates tooltipDates={DataTooltipDates}>
+						<span className={classNames(classNameText, styles.text)}>
+							01.03.23 - 08.03.23
+						</span>
+					</MyTooltip>
+					<span className={classNames(classNameArrow, styles.arrow)}>
+						<img
+							src={arrowIcon}
+							alt="Arrow Icon"
+						/>
+					</span>
+				</div>
+
 			}
-			<MyTooltip isDates tooltipDates={DataTooltipDates}>
-				<span className={classNames(classNameText, styles.text)}>
-					01.03.23 - 08.03.23
-				</span>
-			</MyTooltip>
-			<span className={classNames(classNameArrow, styles.arrow)}>
-				<img
-					src={arrowIcon}
-					alt="Arrow Icon"
-				/>
-			</span>
-		</div>
+		</>
 	)
 }
--- src/components/Cruise/CruiseYourChoose/cardRight/cardRight.tsx
+++ src/components/Cruise/CruiseYourChoose/cardRight/cardRight.tsx
@@ -1,35 +1,57 @@
 import { B, Button } from '../../../../components'
 
 import { ShipName } from '../../../Ship'
-import { CruiseLogo, CruisePrice } from '../../../Cruise'
+import { CruiseDates, CruiseLogo } from '../../../Cruise'
+
+import passengersIcon from '../images/adult.svg'
 
 import styles from './card-right.module.scss'
 import classNames from 'classnames'
+import { useWindowSize } from '../../../../hooks'
 
 export const CardRight = () => {
+	const { isMobile } = useWindowSize()
+
+
+	const isShowBlock = !isMobile
 	return (
 		<>
+			{isMobile && <CruiseLogo className={styles.logo} />}
+			{isMobile && <ShipName isIcon />}
 			<div className={styles.row}>
 				<div className={styles.col}>
-					<B fontWeight={600} className={styles.cabins}>
-						Каюта с балконом
-					</B>
-					<span className={styles.currentCabins}>
-						Каюта с балконом с ограниченным видом
-					</span>
+					{isMobile &&
+						<span className={styles.cabinsCode}>
+							A1
+						</span>
+					}
+					<div>
+						<B fontWeight={600} className={styles.cabins}>
+							Каюта с балконом
+						</B>
+						<span className={styles.currentCabins}>
+							Каюта с балконом с ограниченным видом
+						</span>
+					</div>
 				</div>
 				<div className={styles.col}>
 					<Button className={styles.changeButton}>
 						изменить
 					</Button>
 				</div>
-				<div className={styles.col}>
-					<CruiseLogo className={styles.logo} />
-				</div>
+				{isShowBlock &&
+					<div className={styles.col}>
+						<CruiseLogo className={styles.logo} />
+					</div>
+				}
 			</div>
-
 			<div className={styles.row}>
 				<div className={styles.col}>
+					{isMobile &&
+						<span className={styles.passengersIcon}>
+							<img src={passengersIcon} alt="Passengers icon" />
+						</span>
+					}
 					<B fontWeight={600} className={styles.passengers}>
 						2 взрослых, 1 ребенок
 					</B>
@@ -39,28 +61,34 @@ export const CardRight = () => {
 						изменить
 					</Button>
 				</div>
-				<div className={styles.col}>
-					<ShipName classNameText={styles.shipName} />
-				</div>
+				{isShowBlock &&
+					<div className={styles.col}>
+						<ShipName classNameText={styles.shipName} />
+					</div>
+				}
 			</div>
 			<div className={styles.row}>
 				<div className={styles.col}>
-					<B fontWeight={600} className={styles.date}>
-						01.03.2023-08.03.2023
-					</B>
+					<CruiseDates classComponent={styles.dateWrapper} classNameText={styles.date} isOnlyStartDate />
 				</div>
 				<div className={styles.col}>
 					<Button className={styles.changeButton}>
 						изменить
 					</Button>
 				</div>
-				<div className={styles.col}>
-					<B fontWeight={700} className={styles.price}>
-						1 725 $
-					</B>
-				</div>
+				{isShowBlock &&
+					<div className={styles.col}>
+						<B fontWeight={700} className={styles.price}>
+							1 725 $
+						</B>
+					</div>
+				}
 			</div>
-
+			{isMobile &&
+				<B fontWeight={700} className={styles.price}>
+					1 725 $
+				</B>
+			}
 
 			<div className={styles.buttonsWrapper}>
 				<div className={styles.buttonCol}>
