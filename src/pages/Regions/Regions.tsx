6d837c105fa8c0944b070fdb516618d24422f9a4
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-12-26 15:40:21 +0200

    Add images

--- src/pages/Regions/Regions.tsx
+++ src/pages/Regions/Regions.tsx
@@ -3,8 +3,8 @@ import { Heading } from '../../components'
 export const RegionsPage = () => {
 	return (
 		<div style={{ marginTop: '100px', marginBottom: '100px' }}>
-			<Heading align="center">
-				Страница регионов
+			<Heading align="center" className=''>
+				ВСЕ РЕГИОНЫ
 			</Heading>
 		</div>
 	)
