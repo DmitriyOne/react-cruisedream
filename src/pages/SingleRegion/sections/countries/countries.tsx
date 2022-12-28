3bae1266f0186d6903983d3d236542714127b73d
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2022-12-28 13:51:38 +0200

    Add arrow on mob swiper

--- src/pages/SingleRegion/sections/countries/countries.tsx
+++ src/pages/SingleRegion/sections/countries/countries.tsx
@@ -4,13 +4,16 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { params3Col } from '../../../../fakedata'
 
 import { Link } from 'react-router-dom'
-import { CardBlackout, Container, Heading } from '../../../../components'
+import { Button, CardBlackout, Container, Heading } from '../../../../components'
 import { CRUISE_ROUTES } from '../../../../constants'
 import 'swiper/css'
 import styles from './countries.module.scss'
 import { dataCountries } from '../../../../fakedata'
+import { useSwiperButtons } from '../../../../hooks'
 
 export const Countries = () => {
+	const { upDateSwiper, handlerNext, handlerPrev } = useSwiperButtons()
+
 	return (
 		<Container
 			width="full"
@@ -22,9 +25,10 @@ export const Countries = () => {
 				СТРАНЫ
 			</Heading>
 
-			<Container>
+			<Container className={styles.container}>
 				<Swiper
 					className={styles.swiper}
+					onSwiper={upDateSwiper}
 					{...params3Col}
 				>
 					{dataCountries.map(card =>
@@ -33,6 +37,16 @@ export const Countries = () => {
 						</SwiperSlide>
 					)}
 				</Swiper>
+
+				<Button
+					className={classNames(styles.swiperArrow, styles.prev)}
+					onClick={handlerPrev}
+				/>
+				<Button
+					className={classNames(styles.swiperArrow, styles.next)}
+					onClick={handlerNext}
+				/>
+
 			</Container>
 
 			<Link to={CRUISE_ROUTES.EMPTY} className="button">
--- src/pages/SingleRegion/sections/helpful-posts/helpfulPosts.tsx
+++ src/pages/SingleRegion/sections/helpful-posts/helpfulPosts.tsx
@@ -4,13 +4,16 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import { params3Col } from '../../../../fakedata'
 
 import { Link } from 'react-router-dom'
-import { CardBlackout, Container, Heading } from '../../../../components'
+import { Button, CardBlackout, Container, Heading } from '../../../../components'
 import { CRUISE_ROUTES } from '../../../../constants'
 import 'swiper/css'
 import styles from './helpful-posts.module.scss'
 import { dataHelpfulBlogPosts } from '../../../../fakedata'
+import { useSwiperButtons } from '../../../../hooks'
 
 export const HelpfulPosts = () => {
+	const { upDateSwiper, handlerNext, handlerPrev } = useSwiperButtons()
+
 	return (
 		<Container
 			width="full"
@@ -22,9 +25,10 @@ export const HelpfulPosts = () => {
 				ПОЛЕЗНЫЕ СТАТЬИ ИЗ БЛОГА
 			</Heading>
 
-			<Container>
+			<Container className={styles.container}>
 				<Swiper
 					className={styles.swiper}
+					onSwiper={upDateSwiper}
 					{...params3Col}
 				>
 					{dataHelpfulBlogPosts.map(card =>
@@ -37,6 +41,16 @@ export const HelpfulPosts = () => {
 						</SwiperSlide>
 					)}
 				</Swiper>
+
+				<Button
+					className={classNames(styles.swiperArrow, styles.prev)}
+					onClick={handlerPrev}
+				/>
+				<Button
+					className={classNames(styles.swiperArrow, styles.next)}
+					onClick={handlerNext}
+				/>
+
 			</Container>
 
 			<Link to={CRUISE_ROUTES.EMPTY} className="button">
