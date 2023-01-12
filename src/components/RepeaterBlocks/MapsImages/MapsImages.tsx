019a83fa96edafa86522a8c0fd90a8a91d4f695b
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2023-01-12 14:07:39 +0200

    Update map

--- src/components/RepeaterBlocks/MapsImages/MapsImages.tsx
+++ src/components/RepeaterBlocks/MapsImages/MapsImages.tsx
@@ -1,6 +1,12 @@
-import { useWindowSize } from '../../../hooks'
+import classNames from 'classnames'
+import { Link } from 'react-router-dom'
+import { CRUISE_ROUTES } from '../../../constants'
 
-import mapImage from './images/map.jpg'
+import { useWindowSize } from '../../../hooks'
+import { B } from '../../B'
+import { Container } from '../../Container'
+
+import mapImage from './images/map.svg'
 
 import styles from './maps-images.module.scss'
 
@@ -10,14 +16,107 @@ export const MapsImages = () => {
 	if (isMobile) {
 		return null
 	}
-	
+
 	return (
 		<div className={styles.component}>
-			<img
-				className={styles.img}
-				src={mapImage}
-				alt="Map"
-			/>
+			<div className={styles.imageWrapper}>
+				<img
+					className={styles.image}
+					src={mapImage}
+					alt="Map"
+				/>
+				<div className={styles.buttonWrapper}>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.aroundWorld)}
+					>
+						<B fontWeight={700}>
+							КРУГОСВЕТНОЕ ПУТЕШЕСТВИЕ
+						</B>
+					</Link>
+					
+					{/* first row */}
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.northAmerica)}
+					>
+						СЕВЕРНАЯ АМЕРИКА
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.europe)}
+					>
+						ЕВРОПА
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.asiaFarEast)}
+					>
+						АЗИЯ ДАЛЬНИЙ ВОСТОК
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.mediterranean)}
+					>
+						СРЕДИЗЕМНОЕ МОРЕ
+					</Link>
+
+					{/* second row */}
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.centerAmericaAndPanamaCanal)}
+					>
+						АМЕРИКА И ПАНАМСКИЙ КАНАЛ
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.africaAndMidEast)}
+					>
+						АФРИКА И СРЕДНИЙ ВОСТОК
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.pacificIslandsAndHawaii)}
+					>
+						ОСТРОВА ТИХОГО ОКЕАНА И ГАВАИ
+					</Link>
+
+					{/* third row */}
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.southAmericaAndGalapagos)}
+					>
+						ЮжНАЯ АМЕРИКА и ГАЛАПАГОСЫ
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.atlanticIslands)}
+					>
+						АТЛАНТИЧЕСКИЕ ОСТРОВА
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.indianOcean)}
+					>
+						ИНДИЙСКИЙ ОКЕАН
+					</Link>
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.australiaAndNewZealand)}
+					>
+						АВСТРАЛИЯ НОВАЯ ЗЕЛАНДИЯ
+					</Link>
+
+					<Link
+						to={CRUISE_ROUTES.EMPTY}
+						className={classNames(styles.button, styles.popularRegions)}
+					>
+						<B fontWeight={700}>
+							ПОЛЯРНЫЕ РЕГИОНЫ
+						</B>
+					</Link>
+				</div>
+			</div>
 		</div>
 	)
 }
