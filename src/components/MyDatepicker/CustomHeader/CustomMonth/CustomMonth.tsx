0540d344c0c4f8b1d28c4148c028addbd4548164
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2023-01-13 11:55:06 +0200

    Update datepicker FINISH

---
+++ src/components/MyDatepicker/CustomHeader/CustomMonth/CustomMonth.tsx
@@ -0,0 +1,81 @@
+import { FC, FormEvent } from 'react'
+import classNames from 'classnames'
+
+import { getMonth } from 'date-fns'
+
+import { Button } from '../../../Button'
+
+import iconButton from '../../icon/arrow-blue.svg'
+
+import styles from '../custom-header.module.scss'
+
+interface IProps {
+	date: Date
+	decreaseMonth: () => void
+	increaseMonth: () => void
+	prevMonthButtonDisabled: boolean
+	nextMonthButtonDisabled: boolean
+}
+
+const months = [
+	'Январь',
+	'Февраль',
+	'Март',
+	'Апрель',
+	'Май',
+	'Июнь',
+	'Июль',
+	'Август',
+	'Сентябрь',
+	'Октябрь',
+	'Ноябрь',
+	'Декабрь',
+]
+
+export const CustomMonth: FC<IProps> = ({
+	date,
+	decreaseMonth,
+	increaseMonth,
+	prevMonthButtonDisabled,
+	nextMonthButtonDisabled,
+}) => {
+	const handlerYearPrev = (e: FormEvent) => {
+		e.preventDefault()
+		decreaseMonth()
+	}
+
+	const handlerYearNext = (e: FormEvent) => {
+		e.preventDefault()
+		increaseMonth()
+	}
+
+	return (
+		<div className={styles.componentCustomMonth}>
+			<Button
+				className={classNames(styles.buttonArrow, styles.prev)}
+				onClick={(e) => handlerYearPrev(e)}
+				disabled={prevMonthButtonDisabled}
+			>
+				<img
+					className={styles.image}
+					src={iconButton}
+					alt="Arrow icon"
+				/>
+			</Button>
+			<span className={styles.monthText}>
+				{months[getMonth(date)]}
+			</span>
+			<Button
+				className={classNames(styles.buttonArrow, styles.next)}
+				onClick={(e) => handlerYearNext(e)}
+				disabled={nextMonthButtonDisabled}
+			>
+				<img
+					className={styles.image}
+					src={iconButton}
+					alt="Arrow icon"
+				/>
+			</Button>
+		</div>
+	)
+}
--- src/components/MyDatepicker/MyDatepicker.tsx
+++ src/components/MyDatepicker/MyDatepicker.tsx
@@ -1,14 +1,13 @@
-import { FC, FormEvent } from 'react'
+import { FC } from 'react'
 import classNames from 'classnames'
 
 import DatePicker from 'react-datepicker'
 import { ru } from 'date-fns/esm/locale'
-import { getMonth, getYear } from 'date-fns'
-import range from 'lodash/range'
+
+import { CustomHeader } from './CustomHeader/CustomHeader'
 
 import 'react-datepicker/dist/react-datepicker.css'
 import styles from './my-datepicker.module.scss'
-import dayjs from 'dayjs'
 
 interface IProps {
 	componentClassName?: string
@@ -29,7 +28,6 @@ export const MyDatepicker: FC<IProps> = ({
 	endDate,
 	onChange,
 }) => {
-	const years = range(2023, getYear(new Date()) + 10, 1)
 
 	return (
 		<div className={classNames(styles.component, componentClassName)}>
@@ -45,48 +43,10 @@ export const MyDatepicker: FC<IProps> = ({
 
 				placeholderText={placeholder}
 				locale={ru}
-				// dateFormat={'dd.MM.yy'}
+				dateFormat={'dd.MM.yy'}
 
 				showYearDropdown
-				renderCustomHeader={({
-					date,
-					changeYear,
-					changeMonth,
-					decreaseMonth,
-					increaseMonth,
-					prevMonthButtonDisabled,
-					nextMonthButtonDisabled,
-				}) => (
-					<div
-						style={{
-							margin: 10,
-							display: 'flex',
-							justifyContent: 'center',
-						}}
-					>
-						<button onClick={decreaseMonth} disabled={prevMonthButtonDisabled}>
-							{'<'}
-						</button>
-						<select
-							value={getYear(date)}
-							onChange={({ target: { value } }) => changeYear(Number(value))}
-						>
-							{years.map((option) => (
-								<option key={option} value={option}>
-									{option}
-								</option>
-							))}
-						</select>
-
-						<div>
-							{dayjs(date).format('MMMM')}
-						</div>
-
-						<button onClick={increaseMonth} disabled={nextMonthButtonDisabled}>
-							{'>'}
-						</button>
-					</div>
-				)}
+				renderCustomHeader={({ ...params }) => <CustomHeader {...params} />}
 			/>
 		</div>
 	)
