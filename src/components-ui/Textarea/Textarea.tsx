4958d2d749dca6469fcd269cd86b88d255164b8b
Dmitriy <75241552+DmitriyOne@users.noreply.github.com>
2023-01-18 14:08:12 +0200

    Textarea component

---
+++ src/components-ui/Textarea/Textarea.tsx
@@ -0,0 +1,63 @@
+import classNames from 'classnames'
+import { DetailedHTMLProps, FC, ReactNode, TextareaHTMLAttributes } from 'react'
+
+import { ELabelPosition } from '../../model/enums'
+
+import styles from './textarea.module.scss'
+
+export interface IProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
+	componentClassName?: string
+	width?: 'full' | 'half'
+	textareaClassName?: string
+	placeholder?: string
+	name?: string
+	value?: string
+	labelClassName?: string
+	labelText?: string
+	labelPosition?: keyof typeof ELabelPosition
+	onChange?: (e: any) => void
+	children?: ReactNode
+}
+
+export const Textarea: FC<IProps> = ({
+	componentClassName,
+	width = 'full',
+	textareaClassName,
+	placeholder,
+	name,
+	value,
+	labelClassName,
+	labelText,
+	labelPosition,
+	onChange,
+	children,
+	...props
+}) => {
+
+	const componentClass = classNames(styles.component, componentClassName, {
+		[styles.labelLeft]: labelPosition === 'left',
+		[styles.labelRight]: labelPosition === 'right',
+		[styles.labelTop]: labelPosition === 'top',
+		[styles.labelBottom]: labelPosition === 'bottom',
+		[styles.full]: width === 'full',
+		[styles.half]: width === 'half',
+	})
+	const textareaClass = classNames(styles.textarea, textareaClassName)
+	return (
+		<div className={componentClass}>
+			<textarea
+				id={name}
+				name={name}
+				className={textareaClass}
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+				{...props}
+			/>
+			<label htmlFor={name} className={classNames(styles.label, labelClassName)}>
+				{labelText}
+			</label>
+			{children}
+		</div>
+	)
+}
